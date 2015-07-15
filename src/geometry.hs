884f6d9ffd890c6033858a94d2330de1a21b1c5e
Impl0x <alexmflores@gmail.com>
2015-07-15 09:44:04 -0400

    Initial Commit

---
+++ src/geometry.hs
@@ -0,0 +1,40 @@
+module Geometry where
+
+-- Represents a vector in 2-space
+data Vector = Vector Float Float deriving (Eq, Ord, Show)
+
+-- The sum of two vectors
+vAdd :: Vector -> Vector -> Vector
+vAdd (Vector ux uy) (Vector vx vy) = Vector (ux + vx) (uy + vy)
+
+-- The difference of two vectors
+vSub :: Vector -> Vector -> Vector
+vSub (Vector ux uy) (Vector vx vy) = Vector (ux - vx) (uy - vy)
+
+-- Scales the components of a vector independently
+scale :: Vector -> (Float, Float) -> Vector
+scale (Vector x y) (kx, ky) = Vector (x * kx) (y * ky)
+
+-- Scales both components of a vector uniformly
+scaleUniform :: Vector -> Float -> Vector
+scaleUniform u k = scale u (k, k)
+
+-- The squared magnitude of a vector
+sqMag :: Vector -> Float
+sqMag (Vector x y) = x * x + y * y
+ 
+-- The magnitude of a vector
+mag :: Vector -> Float
+mag = sqrt . sqMag
+
+-- The squared distance between two points
+sqDist :: Vector -> Vector -> Float
+sqDist u v = sqMag $ vSub u v
+
+-- The distance between two points
+dist :: Vector -> Vector -> Float
+dist u v = mag $ vSub u v
+
+-- Normalizes a vector
+norm :: Vector -> Vector
+norm u = scaleUniform u (1.0 / mag u)
---
+++ src/poisson.hs
@@ -0,0 +1,151 @@
+module Poisson (
+    Configuration,
+    makeConfig,
+    generatePoints
+) where
+
+import Control.Applicative ((<$>), (<*>))
+import Control.Monad
+import Control.Monad.Random (getRandomR)
+import Data.Array.IArray
+import Data.Map (Map)
+import Generics.Pointless.Combinators (andf)
+import System.Random 
+import qualified Control.Monad.Random
+import qualified Data.Map.Strict as Map
+
+import Geometry
+
+type Rand = Control.Monad.Random.Rand StdGen
+
+data Configuration = Configuration {
+    topLeft         :: Vector,
+    lowerRight      :: Vector,
+    center          :: Vector,
+    dimensions      :: Vector,
+    minDist         :: Float,
+    cellSize        :: Float,
+    ptsPerIter      :: Int,
+    gridWidth       :: Int,
+    gridHeight      :: Int 
+} deriving (Show)
+
+makeConfig :: Vector -> Vector -> Float -> Int -> Configuration
+makeConfig tl lr minDist perIter =
+    let dim@(Vector dimX dimY)= lr `vSub` tl
+        cSize = minDist / sqrt 2.0 in 
+            Configuration { 
+                topLeft = tl,
+                lowerRight = lr,
+                center = tl `vAdd` (scaleUniform dim 0.5),
+                dimensions = dim,
+                minDist = minDist,
+                cellSize = cSize,
+                ptsPerIter = perIter,
+                gridWidth = ceiling (dimX / cSize),
+                gridHeight = ceiling (dimY / cSize) 
+            }
+
+type GridIndex = (Int, Int)
+type GridValue = Maybe Vector
+type Grid = Array GridIndex GridValue 
+
+initGrid :: Int -> Int -> Grid
+initGrid width height = array ((0, 0), (iMax, jMax)) initVal
+    where iMax = width - 1
+          jMax = height - 1
+          initVal = [((i, j), Nothing) | i <- [0..iMax], j <- [0..jMax]]
+
+type ActiveMap = Map Vector Bool
+data GridState = GridState Grid ActiveMap deriving (Show)
+
+-- Adds a point to the given gridstate
+addPoint :: Configuration -> GridState -> Vector -> GridState
+addPoint cfg (GridState grid activeMap) p = GridState newGrid newActiveMap where
+    pIndex = denormalize p (topLeft cfg) (cellSize cfg)
+    newGrid = grid // [(pIndex, Just p)]
+    newActiveMap = Map.insert p True activeMap
+
+-- Toggles the active status of a given base point, returning the updated GridState
+toggleActive :: GridState -> Vector -> GridState
+toggleActive (GridState grid activeMap) basePoint = GridState grid updatedMap
+    where updatedMap = Map.adjust not basePoint activeMap
+
+denormalize :: Vector -> Vector -> Float -> GridIndex 
+denormalize p origin cellSize = mapTuple truncate (xCoord, yCoord)
+    where mapTuple f (a, b) = (f a, f b)
+          (Vector xCoord yCoord) = (p `vSub` origin) `scaleUniform` (1.0 / cellSize)
+
+-- Creates a random value in the range [0.0, 1.0]
+randFloat :: () -> Rand Float
+randFloat () = getRandomR (0.0, 1.0)
+
+-- Creates a random integer in the range [0, max)
+randInt :: Int -> Rand Int
+randInt max = getRandomR (0, max - 1)
+
+-- Ensures a point is within the boundaries set by a given configuration
+inBounds :: Configuration -> Vector -> Bool
+inBounds cfg (Vector x y) = minX <= x && x < maxX && minY <= y && y < maxY
+    where (Vector minX minY) = topLeft cfg
+          (Vector maxX maxY) = lowerRight cfg
+
+-- Creates a grid state with an initial random point
+initGridState :: Configuration -> Rand GridState
+initGridState cfg = do
+    randXY <- (,) <$> randFloat () <*> randFloat () 
+    let offset = scale (dimensions cfg) randXY 
+        p = (topLeft cfg) `vAdd` offset
+        initCoord = denormalize p (topLeft cfg) (cellSize cfg)
+        activeMap = Map.singleton p True
+        grid = (initGrid (gridWidth cfg) (gridHeight cfg)) // [(initCoord, Just p)]
+    if inBounds cfg p then return $ GridState grid activeMap 
+    else initGridState cfg 
+
+-- Checks if a point is too close any other point on the grid
+tooClose :: Configuration -> (GridIndex -> GridValue) -> Vector -> Bool
+tooClose cfg getGridVal p = any isTooClose gridVals where
+    (pxIndex, pyIndex) = denormalize p (topLeft cfg) (cellSize cfg)
+    lowX = max 0 (pxIndex - 2)
+    lowY = max 0 (pyIndex - 2)
+    highX = (min (gridWidth cfg) (pxIndex + 3)) - 1
+    highY = (min (gridHeight cfg) (pyIndex + 3)) - 1 
+    gridVals = [getGridVal (x, y) | x <- [lowX .. highX], y <- [lowY .. highY]] 
+    isTooClose (Just gridVal) = (gridVal `dist` p) < (minDist cfg)
+    isTooClose Nothing        = False 
+
+-- Creates a random offset and adds it to a given base point
+makeRandomPoint :: Configuration -> Vector -> Rand Vector
+makeRandomPoint cfg basePoint = do
+    rScale <- randFloat ()
+    tScale <- randFloat ()
+    let minD = minDist cfg
+        radius = minD + minD * rScale
+        theta = 2.0 * pi * tScale
+        offset = Vector (radius * sin theta) (radius * cos theta) 
+    return $ basePoint `vAdd` offset
+
+-- Picks a base point for the next set of random samples
+pickBasePoint :: [Vector] -> Rand Vector
+pickBasePoint activePoints = liftM (activePoints !!) (randInt $ length activePoints) 
+
+-- Returns all valid random points sampled from the given base point
+sampleFromBasePoint :: Configuration -> GridState -> Vector -> Rand [Vector]
+sampleFromBasePoint cfg (GridState grid _) basePoint = liftM (filter isValidPoint) samples where
+    getGridVal = (grid !)
+    isValidPoint = andf (inBounds cfg, not . tooClose cfg getGridVal)
+    samples = replicateM (ptsPerIter cfg) (makeRandomPoint cfg basePoint)
+
+sample :: Configuration -> Rand GridState -> Rand GridState
+sample cfg gridState = do
+    gState@(GridState _ activeMap) <- gridState
+    let activePoints = map fst . filter snd . Map.assocs $ activeMap 
+    if null activePoints then gridState
+    else do
+        basePoint <- pickBasePoint activePoints
+        samples <- sampleFromBasePoint cfg gState basePoint 
+        if null samples then sample cfg . return $ toggleActive gState basePoint
+        else sample cfg . return $ foldl (addPoint cfg) gState samples
+
+generatePoints :: Configuration -> Rand GridState
+generatePoints cfg = sample cfg . initGridState $ cfg
